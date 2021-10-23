14a1614c5fa8fe117250f79fe1eba591b9f02d2a
Jivin Vaidya <jvinvaidya@Jivins-MacBook-Air.local>
2021-10-23 14:09:58 +0530

    first commit

---
+++ public/About/check.js
@@ -0,0 +1,18 @@
+// Listen for form submit
+document.getElementById('contact-form').addEventListener('submit',submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+
+// Get values
+	var email= getInputVal('email');
+	
+    console.log(email)
+	
+
+}
+
+// Function to get form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
---
+++ public/Journal/articles/a1/JS/a1.js
@@ -0,0 +1,11 @@
+//////////////// SEARCH BUTTON JS ///////////
+
+
+
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
